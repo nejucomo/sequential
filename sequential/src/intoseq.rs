cbd5feff8ae026162a890f3d9ec3c590faa3446f
user <nobody@nowhere>
2023-10-02 10:12:05 -0700

    Provide docs for all public items.

--- sequential/src/intoseq.rs
+++ sequential/src/intoseq.rs
@@ -1,10 +1,14 @@
 use crate::Sequential;
 
+/// Types which can be converted into a [Sequential] process with specific input, output, and termination types
+///
+/// A blanked implementation ensures all [Sequential] types provide [IntoSequential], analogous to [Iterator] and [IntoIterator].
 pub trait IntoSequential<I> {
     type Output;
     type Terminal;
     type Seq: Sequential<I>;
 
+    /// Convert `self` into a [Sequential] type
     fn into_sequential(self) -> Self::Seq;
 }
 
--- sequential/src/sequential.rs
+++ sequential/src/sequential.rs
@@ -1,8 +1,14 @@
 use either::Either;
 
+/// A [Sequential] process that processes inputs of type `I`, and produces a sequence of `Output` values or a `Terminal`
+///
+/// Implementors only need to provide [Sequential::into_next_with].
 pub trait Sequential<I>: Sized {
     type Output;
     type Terminal;
 
+    /// Consume the [Sequential] and an `input` to produce either a continuation (type `Self`) with an `Output` or else a `Termination` value
+    ///
+    /// This uses move semantics (consuming the [Sequential] and potentially producing a new one) to ensure in the case of termination, no inconsistent sequencing state remains.
     fn into_next_with(self, input: I) -> Either<(Self, Self::Output), Self::Terminal>;
 }
