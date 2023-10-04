03bd9e263b8a623bf9694c2ee293d489d83d98f8
nate <nathan+dev@electriccoin.co>
2023-10-04 13:19:13 -0700

    Remove `seqgen` and collapse from workspace to single crate.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,5 @@
 #![deny(unsafe_code, unused, missing_docs)]
 //! The [Sequential] trait and supporting types for abstract sequential processing over inputs, outputs, and explicit termination
-//!
-//! # Related Crates
-//! - `seqgen` - provides the `SeqGen` trait which is analogous to [Sequential] except with no inputs.
-//! - `sequential-async` - provies an asynchronous equivalent to this crate.
 mod intoseq;
 mod sequential;
 
