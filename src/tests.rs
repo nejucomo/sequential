534e7adf91adb7526dd1679ee2211b543b457d12
nate <nathan+dev@electriccoin.co>
2023-10-13 11:17:27 -0700

    Add crate-level advice for method name disambiguation; add test of blanket `Iterator`→`Sequential`→`IntoSequential` path.

--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,4 @@
-use crate::Sequential;
+use crate::{IntoSequential, Sequential};
 use test_case::test_case;
 
 #[test_case(0..3 => () ; "iter-to-sequential-via-blanket")]
@@ -23,6 +23,18 @@ where
 {
     let mut acc = 0;
     let term = seq.for_each(|n| acc += n);
+    assert_eq!(acc, 3);
+    term
+}
+
+#[test_case(0..3 => () ; "iter-to-sequential-via-blanket")]
+#[test_case((0..1).and_then(1..3) => ((), ()) ; "iter-to-sequential-via-blanket-and-then")]
+fn into_sequential_for_each<S, T>(seq: S) -> T
+where
+    S: IntoSequential<Output = u32, Terminal = T>,
+{
+    let mut acc = 0;
+    let term = seq.into_sequential().for_each(|n| acc += n);
     assert_eq!(acc, 3);
     term
 }
@@ -55,4 +67,12 @@ fn from_fn_mut() {
     assert_eq!(10, n);
     let errmsg = s4.into_next().right().unwrap();
     assert_eq!("overflow: 15 > 10", &errmsg);
+}
+
+#[test]
+fn iterator_for_each_fqsyntax() {
+    let it = 0..5;
+    let mut acc = 0;
+    let () = Sequential::for_each(it, |inc| acc += inc);
+    assert_eq!(acc, 10);
 }
